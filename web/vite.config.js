2cc5abf3e2bc49aeb25491ce111e88b64cb23702
The Coder <rupeshcoder7067@gmail.com>
2026-02-14 21:15:33 +0530

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/authentication/",
+  base: "/",
   server: {
     port: 80, // Default HTTP port taaki :5173 na likhna pade
   }
