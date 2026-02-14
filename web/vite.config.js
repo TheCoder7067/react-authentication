9c3eb5e0fbb629080835190dff98b18353f3c229
The Coder <rupeshcoder7067@gmail.com>
2026-02-14 21:17:45 +0530

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    port: 80, // Default HTTP port taaki :5173 na likhna pade
-  }
 })
