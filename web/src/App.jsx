345339e63a63066aa9155172f35ce67439d21984
The Coder <rupeshcoder7067@gmail.com>
2026-02-14 21:17:33 +0530

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="main-root">
       <Toaster/>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <Routes>
             <Route path='/' element={<Authentication/> }/>
 
